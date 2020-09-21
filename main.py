4a35e30085f1a33aae06a57dc559ed1387b651e1
j s <j.s.98@outlook.com>
2020-09-21 00:35:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random, string
+from flask import Flask, render_template
+
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
+ok_chars = string.ascii_letters + string.digits
+
+@app.route('/')  # '/' for the default page
+def home():
+	return "Wow this is a basic output!"
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
